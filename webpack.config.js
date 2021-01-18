e9a15db483e7dac21aaa98da4ef6ad4627509883
adyang94 <adyang94@yahoo.com>
2021-01-18 15:54:01 -0600

    setting up files

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-undef */
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'source-map',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
